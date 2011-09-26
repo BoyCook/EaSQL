a856a3342dab0d7f43d42a6bfb7e9a4bd0776622
Craig Cook <boycook@me.com>
2011-09-26 14:32:16 +0100

    Added test for domain objects

---
+++ src/test/java/org/cccs/easql/domain/TestDomainObjects.java
@@ -0,0 +1,40 @@
+package org.cccs.easql.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * User: boycook
+ * Date: 26/09/2011
+ * Time: 14:24
+ */
+public class TestDomainObjects {
+
+    @Test
+    public void equalsAndHashCodeShouldWorkForLinkTableWhenObjectsAreTheSame() {
+        final LinkTable table1 = new LinkTable("link1", "left", "right");
+        final LinkTable table2 = new LinkTable("link1", "left", "right");
+        assertTrue(table1.equals(table2));
+    }
+
+    @Test
+    public void equalsAndHashCodeShouldFailForLinkTableWhenNamesAreDifferent() {
+        final LinkTable table1 = new LinkTable("link1", "left", "right");
+        final LinkTable table2 = new LinkTable("link2", "left", "right");
+        assertFalse(table1.equals(table2));
+    }
+
+    @Test
+    public void equalsAndHashCodeShouldFailForLinkTableWhenObjectIsNull() {
+        final LinkTable table1 = new LinkTable("link1", "left", "right");
+        assertFalse(table1.equals(null));
+    }
+
+    @Test
+    public void equalsAndHashCodeShouldFailForLinkTableWhenClassTypesAreDifferent() {
+        final LinkTable table1 = new LinkTable("link1", "left", "right");
+        assertFalse(table1.equals(Cat.class));
+    }
+}
